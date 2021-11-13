7c7ee7be7b806a7528daa0ec6a51a76af80c82f1
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-13 13:22:22 -0600

    Added an About page

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php
+    include("shared.php");
+?>
+
+<?php
+    echo "$HTMLHeader";
+    echo "$header";
+?>
+
+<h1>Our Story</h1>
+
+<p>Since May 2008, we've helped countless customers look their best. Whether you're getting ready for an interview, going on a date, or just in need of a trim, we have the skills and experience to help you look like a million bucks.</p>
+
+<br>
+
+<p>We opened Vintage Barbershop with a mission to offer a classy, premium barber shop experience to the people of Mansfield. When we first opened, we didn't even have a sign - just a big balloon with "Barbershop" written on it.</p>
+
+<br>
+
+<p>We weren't experts about running a business, but we were obsessed with providing the best barber shop experience in Mansfield.</p>
+
+<br>
+
+<p>Over a decade later, we're still just as passionate about helping you look your best.</p>
+
+<?php echo "$BookBtn";
+      echo "$footer";
+?>
