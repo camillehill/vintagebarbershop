85bc92c07823b00879b291d2bde7547eb207bf21
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-13 11:15:01 -0600

    Create booking.php
    Added a page for booking. Just a simple form. Needs a php script for processing - will check our form processing homework for an easy copy-paste.

---
+++ booking.php
@@ -0,0 +1,28 @@
+<?php
+    include("shared.php");
+    echo $HTMLHeader;
+    echo $header;
+?>
+
+<h1>Book an Appointment</h1>
+
+* Required fields
+ <form action= "booking_script.php" method="post">
+	Name * : <input type="text" name="author"><br/>
+	Email : <input type="text" name="email"><br/>
+	Phone number * : <input type="text" name="title"><br/>
+
+
+  Type of service * :
+  <select name="service_type">
+    <option value="haircut">Haircut</option>
+    <option value="beardtrim">Beard trim</option>
+    <option value="shave">Hot lather shave</option>
+  </select>
+
+	Special Instructions : <br/><textarea name="specialinstructions" rows="5" cols="40"></textarea><br>
+	<input type="Submit" name="SubmitThis" value="Preview">
+  </form>
+
+
+<?php echo $footer ?>
