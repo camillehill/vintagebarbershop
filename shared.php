bb3474c57302c3fcf4a9aa83a27be4736aa41324
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-14 17:45:25 -0600

    Disclaimer

--- shared.php
+++ shared.php
@@ -40,7 +40,7 @@ $footer =
         <a href=\"https://www.yelp.com\"></a>
     </div>
     <hr>
-    <p>© 2021 Vintage Barbershop, LLC | Disclaimer: this website was created an Internet Marketing Communication course in Fall 2021, for educational purposes only. Visit <a href='https://www.thevintage-barbershop.com/'>Vintage Barbershop.com</a> for the actual company website.</p>
+    <p>© 2021 Vintage Barbershop, LLC | Disclaimer: this website was created for the Internet Marketing Communication course in Fall 2021, for educational purposes only. Visit <a href='https://www.thevintage-barbershop.com/'>Vintage Barbershop.com</a> for the actual company website.</p>
 </footer>
 </html>";
 
