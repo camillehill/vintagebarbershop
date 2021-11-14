90501fbb3738f5acc4d251a663885f5f0e156883
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-14 11:46:10 -0600

    Styling index.php

--- index.php
+++ index.php
@@ -59,35 +59,54 @@
         <section class="location-hours">
 
             <!-- Left side: Google maps iFrame -->
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13445.903655362958!2d-97.1414829!3d32.5934983!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x627fc974e11233b2!2sVintage%20Barbershop%20LLC!5e0!3m2!1sen!2sus!4v1636830523506!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13445.903655362958!2d-97.1414829!3d32.5934983!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x627fc974e11233b2!2sVintage%20Barbershop%20LLC!5e0!3m2!1sen!2sus!4v1636830523506!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+          <br>
           <!-- Right side: text -->
-          <h2> Location & Hours </h2>
-          <div>
-            <p>26 East Debbie Lane</p>
-            <p>Mansfield, TX 76063</p>
-            <p>(682) 518-0999</p>
-          </div>
-          <div>
-            <p>Tuesday-Friday</p>
-            <p>9AM-6PM</p>
+          <div id="right_side_LH">
+
+            <div id="location">
+            <h2> Location</h2>
+
+              <p>26 East Debbie Lane</p>
+              <p>Mansfield, TX 76063</p>
+              <p>(682) 518-0999</p>
+            </div>
+
             <br>
-            <p>Saturday</p>
-            <p>9AM-4PM</p>
-            <br>
-            <p>Sunday & Monday</p>
-            <p>Closed</p>
+            <div id="hours">
+            <h2>Hours</h2>
+
+              <p>Tuesday-Friday</p>
+              <p>9AM-6PM</p>
+              <br>
+              <p>Saturday</p>
+              <p>9AM-4PM</p>
+              <br>
+              <p>Sunday & Monday</p>
+              <p>Closed</p>
+            </div>
           </div>
         </section>
+        <h2>Our Staff</h2>
         <section class="our-staff">
-          <h2>Our Staff</h2>
+
+          <div id="holly">
             <img src="img/employees/holly_418x418.png">
             <figcaption>Holly</figcaption>
+          </div>
+          <div id="penni">
             <img src="img/employees/penni_418x418.png">
             <figcaption>Penni</figcaption>
+          </div>
+          <div id="sean">
             <img src="img/employees/sean_418x418.png">
             <figcaption>Sean</figcaption>
+          </div>
+          <div id="kendra">
             <img src="img/employees/kendra_418x418.png">
             <figcaption>Kendra</figcaption>
+          </div>
         </section>
         <section class="keeping_mansfield">
           <h2>Keeping Mansfield Looking sharp since 2008</h2>
