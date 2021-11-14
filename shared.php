7d41d089ac9285d171b330695701f5f00884c11c
Camille Hill <87834073+camillehill@users.noreply.github.com>
2021-11-14 10:09:33 -0600

    Revised link to css stylesheet

--- shared.php
+++ shared.php
@@ -6,7 +6,7 @@ $HTMLHeader =
     <meta charset=\"UTF-8\">
     <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <link rel=\"text/css\" type=\"stylesheet\" href=\"style.css\">
+    <link rel=\"stylesheet\" href=\"style.css\">
     <title>Vintage Barbershop | Mansfield, Dallas-Fort Worth</title>
 </head>";
 
