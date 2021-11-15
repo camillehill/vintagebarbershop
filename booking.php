ddbe262f97b7cd0b9f5359172a5ae4f368fa8378
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-14 19:24:52 -0600

    Working on the booking forms

--- booking.php
+++ booking.php
@@ -4,11 +4,14 @@
     echo $header;
 ?>
 
-<h1>Book an Appointment</h1>
+<h2>Book an Appointment</h2>
 <!--New Customer Form -->
-* Required fields
+<div id="outside">
  <form action= "booking_script.php" method="post">
   <h2> New Customer? Enter your information below for 20% your first haircut!</h2>
+  <br>
+  * Required fields
+  <br>
 	Name * : <input type="text" name="name"><br/>
 	Email : <input type="text" name="email"><br/>
 	Phone number * : <input type="text" name="phonenumber"><br/>
@@ -31,9 +34,13 @@
   <!-- Returning customer form >>
   Will need its own php script, not booking_script.php
   -->
-  * Required fields
+
    <form action= "new_customer_script.php" method="post">
+
     <h2> Returning Customers</h2>
+    <br>
+    * Required fields
+    <br>
 
   	Phone number * : <input type="text" name="phonenumber"><br/>
     Date*: <input type="date" name="date"><br/>
@@ -49,7 +56,7 @@
     <br/><textarea name="specialinstructions" rows="5" cols="40"></textarea><br>
   	<input type="Submit" name="SubmitThis" value="Submit">
     </form>
-
+</div>
 
 
 <?php echo $footer ?>
