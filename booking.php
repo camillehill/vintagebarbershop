214a18cdfeace44f765b182640c10978e656bb27
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-14 18:22:05 -0600

    Creating form for new customers and script to process their input

--- booking.php
+++ booking.php
@@ -5,16 +5,16 @@
 ?>
 
 <h1>Book an Appointment</h1>
-
+<!--New Customer Form -->
 * Required fields
  <form action= "booking_script.php" method="post">
+  <h2> New Customer? Enter your information below for 20% your first haircut!</h2>
 	Name * : <input type="text" name="name"><br/>
 	Email : <input type="text" name="email"><br/>
 	Phone number * : <input type="text" name="phonenumber"><br/>
   Date*: <input type="date" name="date"><br/>
   Time*: <input type="time" name="time"><br/>
-
-
+  Date of birth: <input type="date" name ="DOB"><br/>
 
   Type of service * :
   <select name="service_type">
@@ -27,6 +27,29 @@
   <br/><textarea name="specialinstructions" rows="5" cols="40"></textarea><br>
 	<input type="Submit" name="SubmitThis" value="Submit">
   </form>
+
+  <!-- Returning customer form >>
+  Will need its own php script, not booking_script.php
+  -->
+  * Required fields
+   <form action= "new_customer_script.php" method="post">
+    <h2> Returning Customers</h2>
+
+  	Phone number * : <input type="text" name="phonenumber"><br/>
+    Date*: <input type="date" name="date"><br/>
+    Time*: <input type="time" name="time"><br/>
+    Type of service * :
+    <select name="service_type">
+      <option value="haircut">Haircut</option>
+      <option value="beardtrim">Beard trim</option>
+      <option value="shave">Hot lather shave</option>
+    </select>
+    <br>
+  	Special Instructions :
+    <br/><textarea name="specialinstructions" rows="5" cols="40"></textarea><br>
+  	<input type="Submit" name="SubmitThis" value="Submit">
+    </form>
+
 
 
 <?php echo $footer ?>
