5a1d15ac118e39d5ca08c3a54c7ecdeb7a5fc749
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-28 12:36:13 -0600

    That'll do

--- gallery.php
+++ gallery.php
@@ -6,51 +6,55 @@
     echo $HTMLHeader;
     echo $header;
 ?>
+<body>
+  <main>
+    <h2>Our Work</h2>
+    <!--First 6 images -->
+    <div class="gallerypage">
+      <section class="img_gallery">
+        <img class="img_gallery" src="img/Gallery Images/Top 7 images/top6_6_171x250.png">
+        <img class="img_gallery" src="img/Gallery Images/Top 7 images/top6_4_169x250.png">
+        <img class="img_gallery" src="img/Gallery Images/Top 7 images/top6_1_236x250.png">
+        <img class="img_gallery" src="img/Gallery Images/Top 7 images/top6_2_254x250.png">
+        <img class="img_gallery" src="img/Gallery Images/Top 7 images/top6_3_189x250.png">
+        <img class="img_gallery" src="img/Gallery Images/Top 7 images/top6_5_194x250.png">
+      </section>
 
-<h2>Our Work</h2>
-<!--First 6 images -->
-<section class="img_gallery">
-  <img src="img/Gallery Images/Top 7 images/top6_6_171x250.png">
-  <img src="img/Gallery Images/Top 7 images/top6_4_169x250.png">
-  <img src="img/Gallery Images/Top 7 images/top6_1_236x250.png">
-  <img src="img/Gallery Images/Top 7 images/top6_2_254x250.png">
-  <img src="img/Gallery Images/Top 7 images/top6_3_189x250.png">
-  <img src="img/Gallery Images/Top 7 images/top6_5_194x250.png">
-</section>
+      <h2>Beards</h2>
+      <section class="img_gallery">
+        <img class="img_gallery" src="img/Gallery Images/beards/beard_3_275x206.png">
+        <img class="img_gallery" src="img/Gallery Images/beards/beard_5_220x275.png">
+        <img class="img_gallery" src="img/Gallery Images/beards/beard_1_206x275.png">
+        <img class="img_gallery" src="img/Gallery Images/beards/beard_4_275x275.png">
+        <img class="img_gallery" src="img/Gallery Images/beards/beard_2_206x275.png">
+      </section>
 
-<h2>Beards</h2>
-<section class="img_gallery">
-  <img src="img/Gallery Images/beards/beard_3_275x206.png">
-  <img src="img/Gallery Images/beards/beard_5_220x275.png">
-  <img src="img/Gallery Images/beards/beard_1_206x275.png">
-  <img src="img/Gallery Images/beards/beard_4_275x275.png">
-  <img src="img/Gallery Images/beards/beard_2_206x275.png">
-</section>
+      <h2>Fades</h2>
+      <section class="img_gallery">
+        <img class="img_gallery" src="img/Gallery Images/fades/fade_1_205x275.png">
+        <img class="img_gallery" src="img/Gallery Images/fades/fade_2_733x733.png">
+        <img class="img_gallery" src="img/Gallery Images/fades/fade_6_519x449.png">
+        <img class="img_gallery" src="img/Gallery Images/fades/fade_5_315x352.png">
+        <img class="img_gallery" src="img/Gallery Images/fades/fade_3_860x960.png">
+        <img class="img_gallery" src="img/Gallery Images/fades/fade_7_595x563.png">
+      </section>
 
-<h2>Fades</h2>
-<section class="img_gallery">
-  <img src="img/Gallery Images/fades/fade_1_205x275.png">
-  <img src="img/Gallery Images/fades/fade_2_733x733">
-  <img src="img/Gallery Images/fades/fade_6_519x449.png">
-  <img src="img/Gallery Images/fades/fade_5_315x352.png">
-  <img src="img/Gallery Images/fades/fade_3_860x960.png">
-  <img src="img/Gallery Images/fades/fade_7_595x563.png">
-</section>
+      <h2>Military Cuts</h2>
+      <section class="img_gallery">
+        <img class="img_gallery" src="img/Gallery Images/military cuts/military_2_249x275.png">
+        <img class="img_gallery" src="img/Gallery Images/military cuts/military_3_206x275.png">
+      </section>
 
-<h2>Military Cuts</h2>
-<section class="img_gallery">
-  <img src="img/Gallery Images/military cuts/military_2_249x275.png">
-  <img src="img/Gallery Images/military cuts/military_3_206x275.png">
-</section>
-
-<h2>We welcome children of all ages!</h2>
-<section class="img_gallery">
-  <img src="img/Gallery Images/Kids Cuts/kids_welcome_2_edited.png">
-  <img src="img/Gallery Images/Kids Cuts/kids_welcome_3_edited.png">
-  <img src="img/Gallery Images/Kids Cuts/kids_welcome_5_edited.png">
-  <img src="img/Gallery Images/Kids Cuts/kids_welcome_1_edited.png">
-  <img src="img/Gallery Images/Kids Cuts/kids_welcome_7_edited.png">
-  <img src="img/Gallery Images/Kids Cuts/kids_welcome_6_edited.png">
-</section>
-
+      <h2>We welcome children of all ages!</h2>
+      <section class="img_gallery">
+        <img class="img_gallery" src="img/Gallery Images/Kids Cuts/kids_welcome_2_edited.png">
+        <img class="img_gallery" src="img/Gallery Images/Kids Cuts/kids_welcome_3_edited.png">
+        <img class="img_gallery" src="img/Gallery Images/Kids Cuts/kids_welcome_5_edited.png">
+        <img class="img_gallery" src="img/Gallery Images/Kids Cuts/kids_welcome_1_edited.png">
+        <img class="img_gallery" src="img/Gallery Images/Kids Cuts/kids_welcome_7_edited.png">
+        <img class="img_gallery" src="img/Gallery Images/Kids Cuts/kids_welcome_6_edited.png">
+      </section>
+    </div>
+  </main>
+</body>
 <?php echo $footer ?>
