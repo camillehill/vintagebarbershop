f618cae0ef941bb9b2f08b6dbec3e97df3616526
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-11-28 10:16:04 -0600

    CSS for new_or_returning.php isn't working no matter what I try, I'm giving up lol

--- new_or_returning.php
+++ new_or_returning.php
@@ -1,18 +1,25 @@
 <?php
     include("shared.php");
-    echo $HTMLHeader;
-    echo $header;
 ?>
-<h2>Are you a new or returning customer?</h2>
 
+<?php
+    echo "$HTMLHeader";
+    echo "$header";
+?>
 
-  <h3>New customers</h3>
-  <a href="booking_new_customer.php">Book Now</a>
+<body>
+    <main>
+      <h2>Are you a new or returning customer?</h2>
 
-<br>
+        <h3>New customers</h3>
+        <a href="booking_new_customer.php">Book Now</a>
 
-  <h3>Returning customers</h3>
-  <a href="booking_returning_customer.php">Book Now</a>
+        <br>
 
+        <h3>Returning customers</h3>
+        <a href="booking_returning_customer.php">Book Now</a>
+
+  </main>
+</body>
 
 <?php echo $footer ?>
