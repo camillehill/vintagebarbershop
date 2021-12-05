622457e14794466468952dfeb9f4e5107d395793
Darin2 <49596720+Darin2@users.noreply.github.com>
2021-12-05 16:15:53 -0600

    Styling New Customer Form

--- booking_new_customer.php
+++ booking_new_customer.php
@@ -7,28 +7,27 @@
 <h2>Book an Appointment</h2>
 <!--New Customer Form -->
 <div id="outside">
- <form action= "booking_script.php" method="post">
+ <form class="single-column-container" action= "booking_script.php" method="post">
   <h2> New Customer?</h2>
   <h4>Enter your information below for 20% off your first haircut!</h4>
+  <p>* Required fields</p>
   <br>
-  * Required fields
-  <br>
-	Name * : <input type="text" name="name"><br/>
-	Email* : <input type="text" name="email"><br/>
-	Phone number * : <input type="text" name="phonenumber"><br/>
-  Date*: <input type="date" name="date"><br/>
-  Time*: <input type="time" name="time"><br/>
-  Date of birth: <input type="date" name ="DOB"><br/>
+	<label for="name">First and Last Name *</label><input type="text" name="name" placeholder = "Dak Prescott"><br/>
+	<label for="email">Email *</label><input type="text" name="email" placeholder="name@email.com"><br/>
+	<label for="phone-number">Phone number *</label> : <input type="text" name="phonenumber" placeholder="123-456-7890"><br/>
+  <label for="date">Date *</label> <input type="date" name="date"><br/>
+  <label for="time">Time *</label> <input type="time" name="time"><br/>
+  <label for="date of birth">Date of birth *</label><input type="date" name ="DOB"><br/>
 
-  Type of service * :
+  <label for="type of service">Type of service *</label>
   <select name="service_type">
     <option value="haircut">Haircut</option>
     <option value="beardtrim">Beard trim</option>
     <option value="shave">Hot lather shave</option>
   </select>
   <br>
-	Special Instructions :
-  <br/><textarea name="specialinstructions" rows="5" cols="40"></textarea><br>
+  <label for="special instructions">Special Instructions</label>
+  <br/><textarea name="specialinstructions" placeholder = "Start typing....."rows="5" cols="40"></textarea><br>
 	<input type="Submit" name="SubmitThis" value="Submit">
   </form>
 
