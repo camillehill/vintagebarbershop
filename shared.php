5e5ffee62a7874a76f1617ade40ac56a76168ee8
Camille Hill <87834073+camillehill@users.noreply.github.com>
2021-12-05 20:39:59 -0600

    added hamburger icon for mobile navigation with drop menu

--- shared.php
+++ shared.php
@@ -10,9 +10,14 @@ $HTMLHeader =
     <title>Vintage Barbershop | Mansfield, Dallas-Fort Worth</title>
 </head>";
 
+// mobile 
+$logo = "<img class='mobile-logo' src='img/logo/vintage_cropped.png' style='width:150px;' alt='vintage barbershop logo'>";
+$hamburger = "<img src='img/menu.png' alt='menu icon' style='width:25px;'>";
+
+
+// desktop
 $Navigationwithlogo =
-"
-<nav>
+"<nav>
 <img src='img/logo/vintage_cropped.png' style='width:450px;' alt='vintage barbershop logo'>
     <ul class=\"nav-items\">
         <li class=\"nav-link\"><a href=\"index.php\">HOME</a></li>
@@ -33,16 +38,34 @@ $Navigationwithoutlogo =
     </ul>
 </nav>";
 
+$MobileNavigation =
+"<nav class=\"mobi-nav\">
+    <ul class=\"nav-items\">
+        <li class=\"nav-link\"><a class='footerlink' href=\"index.php\">HOME</a></li>
+        <li class=\"nav-link\"><a class='footerlink' href=\"about.php\">ABOUT</a></li>
+        <li class=\"nav-link\"><a class='footerlink' href=\"gallery.php\">GALLERY</a></li>
+        <li class=\"nav-link\"><a class='footerlink' href='new_or_returning.php'>BOOK </a></li>
+    </ul>
+</nav>";
+
 $BookBtn = "<button><a href='new_or_returning.php'>BOOK NOW</a></button>";
-$BookBtnNewCustomer ="<button><a href='booking_new_customer.php'>I'm a New Customer</a></button>";
-$BookBtnReturningCustomer ="<button><a href='booking_returning_customer.php'>I'm a Returning Customer</a></button>";
+$BookBtnNewCustomer ="<button><a href='booking_new_customer.php'>BOOK NOW</a></button>";
+$BookBtnReturningCustomer ="<button><a href='booking_returning_customer.php'>BOOK NOW</a></button>";
 $ViewGalleryBtn = "<button><a href='gallery.php'>View Gallery</a></button>";
+
+$Mobile = 
+"<div id='mobile-nav'>
+    $logo
+    <div class=\"menu\">
+        $hamburger
+        $MobileNavigation
+    </div>   
+</div>";
 
 $header =
 "<header>
     $Navigationwithlogo
-
-
+    $Mobile
 </header>";
 
 $footer =
@@ -56,7 +79,7 @@ $footer =
     </div>
     <hr>
 
-    <p>© 2021 Vintage Barbershop, LLC | Disclaimer: this website was created for the Internet Marketing Communication course in Fall 2021, for educational purposes only. Visit <a class='footerlink' href='https://www.thevintage-barbershop.com/'>Vintage Barbershop.com</a> for the actual company website. View our privacy policy <a class='footerlink' href='https://www.termsfeed.com/live/c5414913-4b96-4802-9b17-eddf4e5778fa'>here</a>.</p>
+    <p>© 2021 Vintage Barbershop, LLC | Disclaimer: this website was created for the Internet Marketing Communication course in Fall 2021, for educational purposes only. Visit <a href='https://www.thevintage-barbershop.com/'>Vintage Barbershop.com</a> for the actual company website. View our privacy policy <a href='https://www.termsfeed.com/live/c5414913-4b96-4802-9b17-eddf4e5778fa'>here</a>.</p>
 </footer>
 </html>";
 
